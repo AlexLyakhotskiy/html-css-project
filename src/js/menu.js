510e698b44b0883669a62b3e7b925f423eec820b
Oleksii Biletskyi <alexbily@gmail.com>
2021-06-22 22:24:16 +0300

    added animation to burger menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,13 +1,17 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
-  const mobileBtnClose = document.querySelector("[data-menu-close]");
+  const body = document.querySelector("body");
 
   menuBtnRef.addEventListener("click", () => {
     mobileMenuRef.classList.toggle("is-open");
+    menuBtnRef.classList.toggle("is-open");
+    body.classList.toggle("is-open");
+
   });
 
-  mobileBtnClose.addEventListener("click", () => {
-    mobileMenuRef.classList.toggle("is-open");
-  });
+  // mobileBtnClose.addEventListener("click", () => {
+  //   mobileMenuRef.classList.toggle("is-open");
+  //   menuBtnRef.classList.toggle("is-open");
+  // });
 })();
