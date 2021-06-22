e0ff77f3fca73deb647c3ffba11c19f1787fe4c3
Nikita Shpanko <shpancore@gmail.com>
2021-06-22 22:45:31 +0300

    added new version of menu script

--- src/js/our.js
+++ src/js/our.js
@@ -2,10 +2,13 @@
     // MENU:
     const menuBtnRef = document.querySelector('[data-menu-button]');
     const mobileMenuRef = document.querySelector('[data-menu]');
+    const body = document.querySelector('body');
     const mobileBtnsCloseArr = document.querySelectorAll('[data-menu-close]');
 
     menuBtnRef.addEventListener('click', () => {
         mobileMenuRef.classList.toggle('is-open');
+        menuBtnRef.classList.toggle('is-open');
+        body.classList.toggle('is-open');
     });
 
     mobileBtnsCloseArr.forEach(el =>
